b74d5c5296bb02f0b552aadbbc1b7b774c2d8874
marcosgd <marcosgonzalezdiaz@gmail.com>
2016-07-12 14:36:55 +0200

    xsd to java 0.0.1
    ejemplo jaxb 2.1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Generando objetos del dominio xsd to java con jaxb
+ 
+# Para instalar en repositorio la libreria ejecutar 
+
+
---
+++ mvnInstalLib.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=./dist/tecmaral-domain-user-0.0.1.jar -DgroupId=com.tecmaral.domain -DartifactId=tecmaral-domain-user -Dversion=0.0.1 -Dpackaging=jar -DgeneratedPom=true
